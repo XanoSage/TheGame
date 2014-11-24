7618a56b17bf40673679c220db04dd80db621b98
Alexander Khramtsov <a.khramtsov@patriotic.org>
2014-11-24 15:06:25 +0200

    - story from docs: GAM-9 - done;

---
+++ TheGame/Assets/Project/Content/Scripts/UI/MainAppViewController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class MainAppViewController : MonoBehaviour {
+
+	#region Variables
+
+	private MainAppViewModel _model;
+
+	#endregion
+
+	#region MonoBehaviour action
+	// Use this for initialization
+	void Start ()
+	{
+
+		_model = GetComponent<MainAppViewModel>();
+
+		if (null == _model)
+		{
+			throw new MissingComponentException("MainAppViewController.Start - cann't find MainAppViewModel for _model");
+		}
+
+		SubscribeEvents();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	#endregion
+
+	#region Actions
+	
+	private void SubscribeEvents()
+	{
+		_model.ChangeShowTimeButton.onClick = OnChangeShowTimeClick;
+		_model.ChangeShowTimeButton.onPress = OnChangeShowTimePressed;
+	}
+
+	private void UnsubscribeEvents()
+	{
+
+	}
+
+	#region Button Handler Actions
+
+	private void OnChangeShowTimeClick(GameObject sender)
+	{
+		Debug.Log("MainAppViewController.OnChangeShowTimeClick - OK");
+	}
+
+	private void OnChangeShowTimePressed(GameObject sender, bool isPressed)
+	{
+		Debug.Log("MainAppViewController.OnChangeShowTimePressed - OK");
+
+		if (isPressed)
+		{
+			_model.ShowTimePressedContainer.gameObject.SetActive(true);
+		}
+		else
+		{
+			_model.ShowTimePressedContainer.gameObject.SetActive(false);
+		}
+	}
+
+	#endregion
+
+	#endregion
+}
---
+++ TheGame/Assets/Project/Content/Scripts/UI/MainAppViewModel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainAppViewModel : MonoBehaviour {
+
+	#region Variables
+
+	public enum MainSetting
+	{
+		Alarm,
+		Timer
+	}
+
+	public UIToggle AlarmToggle;
+	public UIToggle TimerToggle;
+
+	public UILabel AlarmTimeShowLabel;
+	public UILabel TimerTimeShowLabel;
+	public UILabel MessageShowCountLabel;
+
+	public UIEventListener ChangeShowTimeButton;
+	public UIWidget ShowTimeIddleContainer;
+	public UIWidget ShowTimePressedContainer;
+
+	[HideInInspector] public MainSetting Setting;
+
+	#endregion
+}
