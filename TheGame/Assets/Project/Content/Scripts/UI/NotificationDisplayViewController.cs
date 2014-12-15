5cef1625964ef29299df5ab117a818fc85f7db21
Alexander Khramtsov <a.khramtsov@patriotic.org>
2014-12-15 19:50:18 +0200

    - added script for notification panel;

---
+++ TheGame/Assets/Project/Content/Scripts/UI/NotificationDisplayViewController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityTools.Other;
+
+public class NotificationDisplayViewController : MonoBehaviour, IShowable  {
+
+	#region Variables
+
+	private NotificationDisplayViewModel _model;
+
+	#endregion
+
+	#region MonoBehaviour Action
+
+	// Use this for initialization
+	void Start ()
+	{
+		_model = GetComponent<NotificationDisplayViewModel>();
+
+		if (null == _model)
+		{
+			throw new MissingComponentException("NotificationDisplayViewController.Start - cann't find NotificationDisplayViewModel component");
+		}
+
+		SubscribeEvents();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	#endregion
+
+	#region Actions
+
+	public void Init(string chapterName, string description)
+	{
+		_model.ChapterName.text = chapterName;
+		_model.Description.text = description;
+	}
+
+	private void SubscribeEvents()
+	{
+		_model.CloseButton.onClick = OnCloseButtonClick;
+	}
+
+	private void OnCloseButtonClick(GameObject sender)
+	{
+		Debug.Log("NotificationDisplayViewController.OnCloseButtonClick - OK");
+	}
+
+	#endregion
+
+	#region IShowable Implementation
+
+	public void Show()
+	{
+		Visible = true;
+
+		_model.ElementContainer.gameObject.SetActive(true);
+	}
+
+	public void Hide()
+	{
+		Visible = false;
+
+		_model.ElementContainer.gameObject.SetActive(false);
+	}
+
+	public bool Visible { get; private set; }
+
+	#endregion
+}
---
+++ TheGame/Assets/Project/Content/Scripts/UI/NotificationDisplayViewModel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NotificationDisplayViewModel : MonoBehaviour {
+
+	#region Variables
+
+	public UIWidget ElementContainer;
+
+	public UILabel ChapterName;
+	public UILabel Description;
+
+	public UIEventListener CloseButton;
+
+	#endregion
+}
