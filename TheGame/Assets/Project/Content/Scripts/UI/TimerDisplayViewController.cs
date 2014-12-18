830e66ab68e3964c492bf0e428b5dd975f6f2b1b
Alexander Khramtsov <a.khramtsov@patriotic.org>
2014-12-18 14:24:20 +0200

    - timer display - WIP;

---
+++ TheGame/Assets/Project/Content/Scripts/UI/TimerDisplayViewController.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityTools.Other;
+
+public class TimerDisplayViewController : MonoBehaviour, IShowable {
+
+	#region Variables
+
+	private TimerDisplayViewModel _model;
+
+	#endregion
+
+	#region MonoBehaviour Actions
+	// Use this for initialization
+	void Start ()
+	{
+
+		_model = GetComponent<TimerDisplayViewModel>();
+
+		if (null == _model)
+		{
+			throw new MissingComponentException("TimerDisplayViewController.Start - cann't find TimerDisplayViewModel component");
+		}
+
+		SubscribeEvents();
+
+		_model.IsTimerOnPause = false;
+		OnPausePressedAction(_model.IsTimerOnPause);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	#endregion
+
+	#region Actions
+
+	private void SubscribeEvents()
+	{
+		_model.CancelButton.onClick = OnCancelButtonClick;
+		_model.PauseButton.onClick = OnPauseButtonClick;
+	}
+
+	private void OnCancelButtonClick(GameObject sender)
+	{
+		Debug.Log("TimerDisplayViewController.OnCancelButtonClick - OK");
+	}
+
+	private void OnPauseButtonClick(GameObject sender)
+	{
+		Debug.Log("TimerDisplayViewController.OnPauseButtonClick - OK");
+
+		_model.IsTimerOnPause = !_model.IsTimerOnPause;
+
+		OnPausePressedAction(_model.IsTimerOnPause);
+	}
+
+	private void OnPausePressedAction(bool isPaused)
+	{
+		if (isPaused)
+		{
+			_model.PauseLabelIdle.gameObject.SetActive(false);
+			_model.PauseLabelPressed.gameObject.SetActive(false);
+
+			_model.ContinueLabelIdle.gameObject.SetActive(true);
+			_model.ContinueLabelPressed.gameObject.SetActive(true);
+		}
+		else
+		{
+			_model.PauseLabelIdle.gameObject.SetActive(true);
+			_model.PauseLabelPressed.gameObject.SetActive(true);
+
+			_model.ContinueLabelIdle.gameObject.SetActive(false);
+			_model.ContinueLabelPressed.gameObject.SetActive(false);
+		}
+	}
+
+	#endregion
+
+	#region IShowable Implementation
+
+	public void Show()
+	{
+		Visible = true;
+
+		_model.ElementContainer.gameObject.SetActive(true);
+	}
+
+	public void Hide()
+	{
+		Visible = false;
+
+		_model.ElementContainer.gameObject.SetActive(false);
+	}
+
+	public bool Visible { get; private set; }
+
+	#endregion
+}
---
+++ TheGame/Assets/Project/Content/Scripts/UI/TimerDisplayViewModel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerDisplayViewModel : MonoBehaviour {
+
+	#region Variables
+
+	public UIWidget ElementContainer;
+
+	public UIEventListener CancelButton;
+	public UIEventListener PauseButton;
+
+	public UILabel ContinueLabelIdle;
+	public UILabel ContinueLabelPressed;
+
+	public UILabel PauseLabelIdle;
+	public UILabel PauseLabelPressed;
+
+	[HideInInspector] public bool IsTimerOnPause;
+
+	#endregion
+}
